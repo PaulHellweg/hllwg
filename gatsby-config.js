d276f9e700f4efcafc1ae1863af4664bbe8cd57e
Paul Hellweg <49630512+PaulHellweg@users.noreply.github.com>
2020-05-23 19:21:00 +0200

    adds typescript and styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Paul's Bastelbude`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Paul Hellweg`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
