44332c45b09e4c5205c35992d0c5a9f5328ed09b
Paul Hellweg <49630512+PaulHellweg@users.noreply.github.com>
2020-05-31 11:23:12 +0200

    reset website no gatsby any more

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import React from "react";
+
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
