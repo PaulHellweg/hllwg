0bd0fdc295a04bfbaa815d357f7310a1de1d32bc
Paul Hellweg <49630512+PaulHellweg@users.noreply.github.com>
2020-05-31 16:35:34 +0200

    adds styled components and router

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import React from "react";
 
 import logo from "./logo.svg";
+import { ComingSoonPage } from "./views/ComingSoon";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ComingSoonPage />
       </header>
     </div>
   );
---
+++ src/views/ComingSoon.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ComingSoonPage: React.FC = () => {
+  return (
+    <>
+      <h1>coming soon</h1>
+      <h2>Paul's Bastelbude</h2>
+    </>
+  );
+};
